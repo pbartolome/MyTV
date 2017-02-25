d42f85aace009c07d7d4d513a792ab0b7b706746
Pablo Bartolome <pablobart@gmail.com>
2017-02-25 12:02:42 -0600

    Add Playground

---
+++ MyTVPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+
+import IDZSwiftCommonCrypto
+
+let contentPk = "20161217-EPISODE-00004-false"
+let time = String(1484376342)
+let data = contentPk.appending(time)
+let key = "b2zN:Pzf:>WTiC=j$;_e!D.L"
+let hash = HMAC(algorithm: .md5, key: key).update(string: data)?.final()
+
+print(hexString(fromArray: hash!) == "a56629ac2788830f24967fa74dcef888")
+
